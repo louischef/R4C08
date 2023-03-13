40e9503c389dd04c26b1853508ce50242c11f526
louischef <laulouis@D250-S10>
2023-03-13 08:33:02 +0100

    ajout des fichiers

---
+++ client.py
@@ -0,0 +1,67 @@
+import rsa
+import socket
+
+class Client:
+    def __init__(self, port):
+        self.host = socket.gethostname()  # as both code is running on same pc
+        self.port = port
+        self.client_socket = socket.socket()  # instantiate
+        self.bfile = None
+
+    def connect(self):
+        self.client_socket.connect((self.host, self.port))  # connect to the server
+
+    def receiveFile(self):
+        # receive the size of the file
+        expected_size = b""
+        while len(expected_size) < 8:
+            more_size = self.client_socket.recv(8 - len(expected_size))
+            if not more_size:
+                raise Exception("Short file length received")
+            expected_size += more_size
+
+        # Convert to int, the expected file length
+        expected_size = int.from_bytes(expected_size, 'big')
+
+        # Until we've received the expected amount of data, keep receiving
+        self.bfile = b""  # Use bytes, not str, to accumulate
+        while len(self.bfile) < expected_size:
+            buffer = self.client_socket.recv(expected_size - len(self.bfile))
+            if not buffer:
+                raise Exception("Incomplete file received")
+            self.bfile += buffer
+        return self.bfile
+
+    def receiveMessage(self):
+        return self.client_socket.recv(1024).decode()
+
+    def sendMessage(self, msg: str):
+        
+        print("Sending:", msg)
+        self.client_socket.send(str.encode(msg))
+
+    def saveFile(self, bytes: b"", filename: str):
+        with open(filename, 'wb') as f:
+            f.write(self.bfile)
+
+    def generateKeys(self):
+        return rsa.newkeys(512)
+
+    def close(self):
+        if not self.client_socket == None:
+            self.client_socket.close()  # close the connection
+        else:
+            raise Exception("Erreur: la connection a été fermée avant d'être instanciée.")
+
+if __name__ == '__main__':
+    
+    client = Client(5000)
+    (publicKeyC, privKeyC) = client.generateKeys()
+    (publicKeyCE, publicKeyCN) = str(publicKeyC.e), str(publicKeyC.n)
+    client.connect()
+    bfile = client.sendMessage(msg=privkey)
+    f = "output/filename"
+    client.saveFile(bytes=bfile, filename=f)
+    msg = client.receiveMessage()
+    print(msg)
+    client.close()
---
+++ server.py
@@ -0,0 +1,53 @@
+
+import socket
+import rsa
+
+class Server:
+    def __init__(self, port):
+        # get the hostname
+        host = socket.gethostname()
+        self.server_socket = socket.socket()  # get instance
+        # look closely. The bind() function takes tuple as argument
+        self.server_socket.bind((host, port))  # bind host address and port together
+        self.conn = None
+
+    def waitForConnection(self):
+        # configure how many client the server can listen simultaneously
+        self.server_socket.listen(2)
+        self.conn, address = self.server_socket.accept()  # accept new connection
+        print("Connection from: " + str(address))
+
+    def sendMessage(self, msg: str):
+        print("Sending:", msg)
+        self.conn.send(str.encode(msg))
+
+    def receiveMessage(self):
+        return self.conn.recv(1024).decode()
+
+    def sendFile(self, filename: str):
+        print("Sending:", filename)
+        with open(filename, 'rb') as f:
+            raw = f.read()
+        # Send actual length ahead of data, with fixed byteorder and size
+        self.conn.sendall(len(raw).to_bytes(8, 'big'))
+        self.conn.send(raw)  # send data to the client
+
+    def generateKeys(self):
+        return rsa.newkeys(512)
+    def close(self):
+        if not self.conn == None:
+            self.conn.close()  # close the connection
+        else:
+            raise Exception("Erreur: la connection a été fermée avant d'être instanciée.")
+
+
+
+if __name__ == '__main__':
+    server = Server(5000)
+    (publicKeyS, privKeyS) = server.generateKeys()
+    print(privKeyS)
+    print(publicKeyS)
+    (publicKeySE, publicKeySN) = str(publicKeyS.e), str(publicKeyS.n)
+    server.waitForConnection()
+    server.sendMessage(publicKeySE + "/" + publicKeySN)
+    server.close()
